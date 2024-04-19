2089570558590962fc6f47da6564e1e8cb943741
Yuri-ga1 <regota09@gmail.com>
2024-04-19 13:28:11 +0800

    update permission request

--- app/src/main/java/com/example/blindpeoplenavigation/CameraManager.kt
+++ app/src/main/java/com/example/blindpeoplenavigation/CameraManager.kt
@@ -2,36 +2,40 @@ package com.example.blindpeoplenavigation
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.pm.PackageManager
 import android.graphics.SurfaceTexture
-import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCaptureSession
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraDevice
 import android.hardware.camera2.CameraManager
+import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
 import android.view.Surface
 import android.view.TextureView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import android.widget.Toast
 
 class CameraManager(private val context: Context, private val textureView: TextureView) {
 
     private lateinit var cameraDevice: CameraDevice
     private lateinit var cameraManager: CameraManager
     private lateinit var handler: Handler
+    private lateinit var handlerThread: HandlerThread
 
     fun initialize() {
         cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
 
-        val handlerThread = HandlerThread("videoThread")
+        handlerThread = HandlerThread("videoThread")
         handlerThread.start()
         handler = Handler(handlerThread.looper)
 
         textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
             override fun onSurfaceTextureAvailable(surface: SurfaceTexture, width: Int, height: Int) {
-                openCamera()
+                val camera = getCameraId()
+                if (camera != null) {
+                    openCamera(camera)
+                } else {
+                    Toast.makeText(context, "Ошибка: основная камера не найдена", Toast.LENGTH_LONG).show()
+                }
             }
 
             override fun onSurfaceTextureSizeChanged(surface: SurfaceTexture, width: Int, height: Int) {}
@@ -42,44 +46,63 @@ class CameraManager(private val context: Context, private val textureView: Textu
         }
     }
 
+    fun closeCamera() {
+        if (::cameraDevice.isInitialized) {
+            cameraDevice.close()
+        }
+    }
+
     @SuppressLint("MissingPermission")
-    private fun openCamera() {
+    private fun openCamera(cameraId: String) {
+        cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
+            override fun onOpened(camera: CameraDevice) {
+                cameraDevice = camera
+
+                val surfaceTexture = textureView.surfaceTexture
+                val surface = Surface(surfaceTexture)
+
+                val captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequest.addTarget(surface)
+
+                cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(session: CameraCaptureSession) {
+                        session.setRepeatingRequest(captureRequest.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(session: CameraCaptureSession) {}
+                    }, handler)
+            }
+
+            override fun onDisconnected(camera: CameraDevice) {}
+
+            override fun onError(camera: CameraDevice, error: Int) {}
+        }, handler)
+    }
+
+    fun saveCameraState(outState: Bundle) {
+        if (::cameraDevice.isInitialized) {
+            outState.putString("cameraId", cameraDevice.id)
+        }
+    }
+
+    fun restoreCameraState(savedInstanceState: Bundle) {
+        if (savedInstanceState.containsKey("cameraId")) {
+            val cameraId = savedInstanceState.getString("cameraId", null)
+            if (cameraId != null) {
+                openCamera(cameraId)
+            }
+        }
+    }
+
+    private fun getCameraId(): String? {
         val cameraIds = cameraManager.cameraIdList
         for (cameraId in cameraIds) {
             val characteristics = cameraManager.getCameraCharacteristics(cameraId)
             val facing = characteristics.get(CameraCharacteristics.LENS_FACING)
             if (facing == CameraCharacteristics.LENS_FACING_BACK) {
-                cameraManager.openCamera(cameraId, object : CameraDevice.StateCallback() {
-                    override fun onOpened(camera: CameraDevice) {
-                        cameraDevice = camera
-
-                        val surfaceTexture = textureView.surfaceTexture
-                        val surface = Surface(surfaceTexture)
-
-                        val captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
-                        captureRequest.addTarget(surface)
-
-                        cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
-                            override fun onConfigured(session: CameraCaptureSession) {
-                                session.setRepeatingRequest(captureRequest.build(), null, null)
-                            }
-
-                            override fun onConfigureFailed(session: CameraCaptureSession) {}
-                        }, handler)
-                    }
-
-                    override fun onDisconnected(camera: CameraDevice) {}
-
-                    override fun onError(camera: CameraDevice, error: Int) {}
-                }, handler)
-                break
+                return cameraId
             }
         }
-    }
-
-    fun getPermission() {
-        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            (context as AppCompatActivity).requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
-        }
+        return null
     }
 }
--- app/src/main/java/com/example/blindpeoplenavigation/MainActivity.kt
+++ app/src/main/java/com/example/blindpeoplenavigation/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.blindpeoplenavigation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.TextureView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var textureView: TextureView
@@ -16,7 +22,76 @@ class MainActivity : AppCompatActivity() {
 
         textureView = findViewById(R.id.textureView)
         cameraManager = CameraManager(this, textureView)
-        cameraManager.initialize()
-        cameraManager.getPermission()
+
+        if (!hasRequiredPermissions()) {
+            ActivityCompat.requestPermissions(
+                this, CAMERA_PERMITION, CAMERA_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            cameraManager.initialize()
+        }
+        Toast.makeText(this, "create", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (hasRequiredPermissions()) {
+            Toast.makeText(this, "message", Toast.LENGTH_LONG).show()
+            val savedInstanceState = Bundle()
+            onRestoreInstanceState(savedInstanceState)
+        } else {
+            ActivityCompat.requestPermissions(
+                this, CAMERA_PERMITION, CAMERA_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        val outState = Bundle()
+        onSaveInstanceState(outState)
+        cameraManager.closeCamera()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        cameraManager.saveCameraState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        cameraManager.restoreCameraState(savedInstanceState)
+    }
+    private fun hasRequiredPermissions(): Boolean{
+        return CAMERA_PERMITION.all{
+            ContextCompat.checkSelfPermission(
+                applicationContext,
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (hasRequiredPermissions()) {
+                cameraManager.initialize()
+            } else {
+                ActivityCompat.requestPermissions(
+                    this, CAMERA_PERMITION, CAMERA_PERMISSION_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    companion object{
+        private  val CAMERA_PERMITION = arrayOf(
+            Manifest.permission.CAMERA,
+        )
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 101
     }
 }
